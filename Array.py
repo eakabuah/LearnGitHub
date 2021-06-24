a3fac47a949c74b14d41d58b83a40b87bf4c97a5
eakabuah <eakabuah@gmail.com>
2021-06-24 12:55:16 +0000

    -m "added Array2.py"

--- Array.py
+++ Array.py
@@ -3,6 +3,9 @@ from array import *
 vals = array('i', [5,9,8,4,2])
 
 newArr = array(vals.typecode, (a*a for a in vals))
-
+newArr2 = array(vals.typecode, (a*a*a for a in vals))
 for i in newArr:
+    print(i)
+
+for i in newArr2:
     print(i)
